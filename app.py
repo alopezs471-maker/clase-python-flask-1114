bae3d72605f35cce5d703cb2cf34ee34a8a8c26e
sadcat8708 <jhoanprogelves8@gmail.com>
2026-06-03 18:02:50 -0500

    me llamo juan carlos =)

--- app.py
+++ app.py
@@ -1,45 +1,27 @@
-# Importamos Flask y una funcion que permite mostrar un HTML.
 from flask import Flask, render_template
 
-
-# Creamos la aplicacion principal.
-# Este objeto sera el centro de nuestro proyecto Flask.
 app = Flask(__name__)
 
-
-# Cuando alguien entra a la direccion principal del sitio, Flask ejecuta
-# esta funcion y devuelve la pagina `index.html`.
 @app.route("/")
 def inicio():
-    # `render_template` busca archivos dentro de la carpeta `templates`.
-    return render_template("index.html")
+    # Datos del portal
+    nombre_estudiante = "Alejandro"
+    email_estudiante = "Alopezs471@inemkennedy.edu.co"
+    horario = "Miercoles 16:45-18:10 | Jueves 12:30-14:20"
+    aula = "2"
+    descripcion = "Aprenderemos Python, Flask y construiremos un portal web real"
+    
+    # Pasar los datos a la plantilla
+    return render_template(
+        "index.html",
+        profesor=nombre_estudiante,
+        email=email_estudiante,
+        horario=horario,
+        aula=aula,
+        descripcion=descripcion
+    )
+nombre = "ale"
+edad = 20
 
-@app.route("/contacto")
-def contac():
-    # `render_template` busca archivos dentro de la carpeta `templates`.
-    return render_template("contacto.html")
-
-
-@app.route("/recursos")
-def recursos():
-    recursos = [
-    "Entorno virtual",
-    "Rutas en Flask",
-    "Plantillas HTML",
-    "Variables con Jinja"
-]
-    return render_template("recursos.html", recursos=recursos)
-
-@app.route("/acerca")
-def planeta():
-    # `render_template` busca archivos dentro de la carpeta `templates`.
-    return render_template("acerca.html")
-
-
-
-
-# Este bloque se ejecuta solo si corremos `python app.py` desde la terminal.
 if __name__ == "__main__":
-    # `debug=True` sirve en desarrollo porque reinicia el servidor
-    # cuando detecta cambios y muestra errores con mas detalle.
     app.run(debug=True)
